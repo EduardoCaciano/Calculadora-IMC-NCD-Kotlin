bdb4f8721e0975c2a970349693b31e8c11069c9a
EDUARDO DA SILVA CACIANO <20272669@cfp127aluno.local>
2021-05-06 12:41:17 -0300

    envio 06/05/2021- imc finalizado

--- imc-ds2m-a/app/src/main/java/br/senai/sp/jandira/imcapp20_a/CalcularImc.kt
+++ imc-ds2m-a/app/src/main/java/br/senai/sp/jandira/imcapp20_a/CalcularImc.kt
@@ -1,20 +1,26 @@
 package br.senai.sp.jandira.imcapp20_a
 
+import android.util.Log
+
 fun calcularImc(peso: Double, altura: Double) = peso / (altura * altura)
 
-fun obterStatus(imc: Double) : String {
+fun obterStatus(imc: Double) : List<String> {
+
+    var resultados = emptyList<String>()
     if (imc < 18.5) {
-        return "Abaixo do Peso"
+        resultados = listOf("Abaixo do Peso", "Risco de fadiga, stress e ansiedade")
     } else if (imc > 18.5 && imc < 25) {
-        return "Peso Ideal"
+        resultados = listOf("Peso Ideal", "Menor risco de doenças cardiacas e vasculares")
     } else if (imc >= 25 && imc < 30) {
-        return "Levemente acima do peso"
+        resultados = listOf("Levemente acima do peso", "Risco de fadiga, má circulação, varizes")
     } else if (imc >= 30 && imc < 35) {
-        return "Obesidade Grau I"
+        resultados = listOf("Obesidade Grau I", "Risco de diabetes, angina, infarto, aterosclerose")
     } else if (imc >= 35 && imc < 40) {
-        return "Obesidade Grau II"
+        resultados = listOf("Obesidade Grau II", "Risco de apneia do sono, falta de ar")
     } else {
-        return "Obesidade Grau III"
+        resultados = listOf("Obesidade Grau III", "Risco de refluxo, dificuldade para se mover, escaras, diabetes, infarto, AVC")
     }
+
+    return resultados
 }
 
